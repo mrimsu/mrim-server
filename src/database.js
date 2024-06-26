048eaa5cf281bb890b5621c2037627cf0c449a13
mikhail <mikhail@tskau.team>
2024-06-26 14:50:55 +0500

    feat: возможность редачить/удалить контакт

--- src/database.js
+++ src/database.js
@@ -1,9 +1,9 @@
-const config = require("../config");
+const config = require('../config')
 
-const mysql2 = require("mysql2/promise");
-const bcrypt = require("bcrypt");
+const mysql2 = require('mysql2/promise')
+const bcrypt = require('bcrypt')
 
-const pool = mysql2.createPool(config.database.connectionUri);
+const pool = mysql2.createPool(config.database.connectionUri)
 
 /**
  * Получение пользователя при помощи учетных данных
@@ -13,26 +13,26 @@ const pool = mysql2.createPool(config.database.connectionUri);
  *
  * @returns {Promise<number>} ID пользователя
  */
-async function getUserIdViaCredentials(login, password) {
-  const connection = await pool.getConnection();
+async function getUserIdViaCredentials (login, password) {
+  const connection = await pool.getConnection()
 
   // eslint-disable-next-line no-unused-vars
   const [results, _fields] = await connection.query(
-    "SELECT `user`.`id`, `user`.`passwd` FROM `user` WHERE `user`.`login` = ?",
-    [login],
-  );
+    'SELECT `user`.`id`, `user`.`passwd` FROM `user` WHERE `user`.`login` = ?',
+    [login]
+  )
 
   if (results.length === 0) {
-    throw new Error(`пользователь ${login} не найден`);
+    throw new Error(`пользователь ${login} не найден`)
   }
 
-  const [user] = results;
+  const [user] = results
 
   if (!bcrypt.compareSync(password, user.passwd)) {
-    throw new Error("пароль неверный");
+    throw new Error('пароль неверный')
   }
 
-  return user.id;
+  return user.id
 }
 
 /**
@@ -41,19 +41,19 @@ async function getUserIdViaCredentials(login, password) {
  * @param {number} userId ID пользователя
  * @returns {Promise<Array>} Массив групп контактов
  */
-async function getContactGroups(userId) {
-  const connection = await pool.getConnection();
+async function getContactGroups (userId) {
+  const connection = await pool.getConnection()
 
   // eslint-disable-next-line no-unused-vars
   const [results, _fields] = await connection.query(
-    "SELECT `contact_group`.`id`, `contact_group`.`name` " +
-      "FROM `contact_group` " +
-      "WHERE `contact_group`.`user_id` = ? " +
-      "ORDER BY `contact_group`.`idx`",
-    [userId],
-  );
+    'SELECT `contact_group`.`id`, `contact_group`.`name` ' +
+      'FROM `contact_group` ' +
+      'WHERE `contact_group`.`user_id` = ? ' +
+      'ORDER BY `contact_group`.`idx`',
+    [userId]
+  )
 
-  return results;
+  return results
 }
 
 /**
@@ -62,19 +62,20 @@ async function getContactGroups(userId) {
  * @param {number} ownerUserId ID владелец контакта
  * @returns {Promise<Array>} Массив контактов
  **/
-async function getContactsFromGroups(ownerUserId) {
-  const connection = await pool.getConnection();
+async function getContactsFromGroups (ownerUserId) {
+  const connection = await pool.getConnection()
 
   // eslint-disable-next-line no-unused-vars
   const [results, _fields] = await connection.query(
-    "SELECT `contact`.`contact_group_id`, `user`.`id`, `user`.`login` " +
-      "FROM `contact` " +
-      "INNER JOIN `user` ON `contact`.`user_id` = `user`.`id` " +
-      "WHERE `contact`.`owner_user_id` = ?",
-    [ownerUserId],
-  );
+    'SELECT `contact`.`contact_group_id`, `contact`.`nickname` as `contact_nick`, ' +
+      '`user`.`nick`, `user`.`id`, `user`.`login` ' +
+      'FROM `contact` ' +
+      'INNER JOIN `user` ON `contact`.`user_id` = `user`.`id` ' +
+      'WHERE `contact`.`owner_user_id` = ?',
+    [ownerUserId]
+  )
 
-  return results;
+  return results
 }
 
 /**
@@ -83,80 +84,80 @@ async function getContactsFromGroups(ownerUserId) {
  * @param {Object} searchParameters Параметры поиска
  * @returns {Promise<Array>} Массив поиска
  */
-async function searchUsers(searchParameters) {
-  const connection = await pool.getConnection();
+async function searchUsers (searchParameters) {
+  const connection = await pool.getConnection()
   let query =
-    "SELECT `user`.`login`, `user`.`nick`, `user`.`f_name`, `user`.`l_name`, `user`.`location`, " +
-    "`user`.`birthday`, `user`.`zodiac`, `user`.`phone`, `user`.`sex` " +
-    "FROM `user` " +
-    "WHERE ";
-  const variables = [];
+    'SELECT `user`.`login`, `user`.`nick`, `user`.`f_name`, `user`.`l_name`, `user`.`location`, ' +
+    '`user`.`birthday`, `user`.`zodiac`, `user`.`phone`, `user`.`sex` ' +
+    'FROM `user` ' +
+    'WHERE '
+  const variables = []
 
-  if (Object.hasOwn(searchParameters, "login")) {
-    query += "`user`.`login` LIKE ? AND ";
-    variables.push(`%${searchParameters.login}%`);
+  if (Object.hasOwn(searchParameters, 'login')) {
+    query += '`user`.`login` LIKE ? AND '
+    variables.push(`%${searchParameters.login}%`)
   }
 
-  if (Object.hasOwn(searchParameters, "nickname")) {
-    query += "`user`.`nick` LIKE ? AND ";
-    variables.push(`%${searchParameters.nickname}%`);
+  if (Object.hasOwn(searchParameters, 'nickname')) {
+    query += '`user`.`nick` LIKE ? AND '
+    variables.push(`%${searchParameters.nickname}%`)
   }
 
-  if (Object.hasOwn(searchParameters, "firstName")) {
-    query += "`user`.`f_name` LIKE ? AND ";
-    variables.push(`%${searchParameters.firstName}%`);
+  if (Object.hasOwn(searchParameters, 'firstName')) {
+    query += '`user`.`f_name` LIKE ? AND '
+    variables.push(`%${searchParameters.firstName}%`)
   }
 
-  if (Object.hasOwn(searchParameters, "lastName")) {
-    query += "`user`.`l_name` LIKE ? AND ";
-    variables.push(`%${searchParameters.lastName}%`);
-  }
-
-  if (
-    Object.hasOwn(searchParameters, "minimumAge") &&
-    Object.hasOwn(searchParameters, "maximumAge")
-  ) {
-    query += "YEAR(CURDATE()) - YEAR(`user`.`birthday`) BETWEEN ? AND ? AND ";
-    variables.push(searchParameters.minimumAge, searchParameters.maximumAge);
+  if (Object.hasOwn(searchParameters, 'lastName')) {
+    query += '`user`.`l_name` LIKE ? AND '
+    variables.push(`%${searchParameters.lastName}%`)
   }
 
   if (
-    Object.hasOwn(searchParameters, "minimumAge") &&
-    !Object.hasOwn(searchParameters, "maximumAge")
+    Object.hasOwn(searchParameters, 'minimumAge') &&
+    Object.hasOwn(searchParameters, 'maximumAge')
   ) {
-    query += "YEAR(CURDATE()) - YEAR(`user`.`birthday`) >= ? AND ";
-    variables.push(searchParameters.minimumAge);
+    query += 'YEAR(CURDATE()) - YEAR(`user`.`birthday`) BETWEEN ? AND ? AND '
+    variables.push(searchParameters.minimumAge, searchParameters.maximumAge)
   }
 
   if (
-    Object.hasOwn(searchParameters, "maximumAge") &&
-    !Object.hasOwn(searchParameters, "minimumAge")
+    Object.hasOwn(searchParameters, 'minimumAge') &&
+    !Object.hasOwn(searchParameters, 'maximumAge')
   ) {
-    query += "YEAR(CURDATE()) - YEAR(`user`.`birthday`) <= ? AND ";
-    variables.push(searchParameters.maximumAge);
+    query += 'YEAR(CURDATE()) - YEAR(`user`.`birthday`) >= ? AND '
+    variables.push(searchParameters.minimumAge)
   }
 
-  if (Object.hasOwn(searchParameters, "zodiac")) {
-    query += "`user`.`zodiac` = ? AND ";
-    variables.push(searchParameters.zodiac);
+  if (
+    Object.hasOwn(searchParameters, 'maximumAge') &&
+    !Object.hasOwn(searchParameters, 'minimumAge')
+  ) {
+    query += 'YEAR(CURDATE()) - YEAR(`user`.`birthday`) <= ? AND '
+    variables.push(searchParameters.maximumAge)
   }
 
-  if (Object.hasOwn(searchParameters, "birthmonth")) {
-    query += "MONTH(`user`.`birthday`) = ? AND ";
-    variables.push(searchParameters.birthmonth);
+  if (Object.hasOwn(searchParameters, 'zodiac')) {
+    query += '`user`.`zodiac` = ? AND '
+    variables.push(searchParameters.zodiac)
   }
 
-  if (Object.hasOwn(searchParameters, "birthday")) {
-    query += "DAY(`user`.`birthday`) = ? AND ";
-    variables.push(searchParameters.birthday);
+  if (Object.hasOwn(searchParameters, 'birthmonth')) {
+    query += 'MONTH(`user`.`birthday`) = ? AND '
+    variables.push(searchParameters.birthmonth)
+  }
+
+  if (Object.hasOwn(searchParameters, 'birthday')) {
+    query += 'DAY(`user`.`birthday`) = ? AND '
+    variables.push(searchParameters.birthday)
   }
 
   // TODO mikhail КОСТЫЛЬ КОСТЫЛЬ КОСТЫЛЬ
-  query = query.substring(0, query.length - 4) + "LIMIT 50";
+  query = query.substring(0, query.length - 4) + 'LIMIT 50'
 
   // eslint-disable-next-line no-unused-vars
-  const [results, _fields] = await connection.query(query, variables);
-  return results;
+  const [results, _fields] = await connection.query(query, variables)
+  return results
 }
 
 /**
@@ -169,41 +170,41 @@ async function searchUsers(searchParameters) {
  *
  * @returns {Promise<number>} ID пользователя из нового контакта
  */
-async function addContactToGroup(
+async function addContactToGroup (
   ownerUserId,
   groupIndex,
   contactLogin,
-  contactNickname,
+  contactNickname
 ) {
-  const connection = await pool.getConnection();
+  const connection = await pool.getConnection()
 
   const [contactUserResults, groupResults] = await Promise.all([
     connection.query(
-      "SELECT `user`.`id` FROM `user` WHERE `user`.`login` = ?",
-      [contactLogin],
+      'SELECT `user`.`id` FROM `user` WHERE `user`.`login` = ?',
+      [contactLogin]
     ),
     connection.query(
-      "SELECT `contact_group`.`id` FROM `contact_group` " +
-        "WHERE `contact_group`.`user_id` = ? AND `contact_group`.`idx` = ?",
-      [ownerUserId, groupIndex],
-    ),
-  ]);
+      'SELECT `contact_group`.`id` FROM `contact_group` ' +
+        'WHERE `contact_group`.`user_id` = ? AND `contact_group`.`idx` = ?',
+      [ownerUserId, groupIndex]
+    )
+  ])
 
   if (contactUserResults[0].length === 0 || groupResults[0].length === 0) {
-    throw new Error("либо пользователь, либо группа не найдена");
+    throw new Error('либо пользователь, либо группа не найдена')
   }
 
-  const [{ id: contactUserId }] = contactUserResults[0];
-  const [{ id: contactGroupId }] = groupResults[0];
+  const [{ id: contactUserId }] = contactUserResults[0]
+  const [{ id: contactGroupId }] = groupResults[0]
 
   await connection.execute(
-    "INSERT INTO `contact` " +
-      "(`contact`.`contact_group_id`, `contact`.`owner_user_id`, `contact`.`user_id`, `contact`.`nickname`) " +
-      "VALUES (?, ?, ?, ?)",
-    [contactGroupId, ownerUserId, contactUserId, contactNickname],
-  );
+    'INSERT INTO `contact` ' +
+      '(`contact`.`contact_group_id`, `contact`.`owner_user_id`, `contact`.`user_id`, `contact`.`nickname`) ' +
+      'VALUES (?, ?, ?, ?)',
+    [contactGroupId, ownerUserId, contactUserId, contactNickname]
+  )
 
-  return contactUserId;
+  return contactUserId
 }
 
 /**
@@ -214,26 +215,26 @@ async function addContactToGroup(
  *
  * @returns {Promise<number>} Индекс группы контактов
  */
-async function createNewGroup(userId, groupName) {
-  const connection = await pool.getConnection();
+async function createNewGroup (userId, groupName) {
+  const connection = await pool.getConnection()
 
   // eslint-disable-next-line no-unused-vars
   const [countResults, _countFields] = await connection.query(
-    "SELECT COUNT(`contact_group`.`id`) as `contact_group_cnt` " +
-      "FROM `contact_group` " +
-      "WHERE `contact_group`.`user_id` = ?",
-    [userId],
-  );
-  const groupIndex = countResults[0].contact_group_cnt;
+    'SELECT COUNT(`contact_group`.`id`) as `contact_group_cnt` ' +
+      'FROM `contact_group` ' +
+      'WHERE `contact_group`.`user_id` = ?',
+    [userId]
+  )
+  const groupIndex = countResults[0].contact_group_cnt
 
   await connection.execute(
-    "INSERT INTO `contact_group` " +
-      "(`contact_group`.`user_id`, `contact_group`.`name`, `contact_group`.`idx`) " +
-      "VALUES (?, ?, ?)",
-    [userId, groupName, groupIndex],
-  );
+    'INSERT INTO `contact_group` ' +
+      '(`contact_group`.`user_id`, `contact_group`.`name`, `contact_group`.`idx`) ' +
+      'VALUES (?, ?, ?)',
+    [userId, groupName, groupIndex]
+  )
 
-  return groupIndex;
+  return groupIndex
 }
 
 /**
@@ -243,15 +244,15 @@ async function createNewGroup(userId, groupName) {
  * @param {number} groupIndex Индекс группы
  * @param {string} groupName Новое имя группы
  */
-async function modifyGroupName(userId, groupIndex, groupName) {
-  const connection = await pool.getConnection();
+async function modifyGroupName (userId, groupIndex, groupName) {
+  const connection = await pool.getConnection()
 
   await connection.execute(
-    "UPDATE `contact_group` " +
-      "SET `contact_group`.`name` = ? " +
-      "WHERE `contact_group`.`user_id` = ? AND `contact_group`.`idx` = ?",
-    [groupName, userId, groupIndex],
-  );
+    'UPDATE `contact_group` ' +
+      'SET `contact_group`.`name` = ? ' +
+      'WHERE `contact_group`.`user_id` = ? AND `contact_group`.`idx` = ?',
+    [groupName, userId, groupIndex]
+  )
 }
 
 /**
@@ -260,21 +261,51 @@ async function modifyGroupName(userId, groupIndex, groupName) {
  * @param {number} userId ID пользователя
  * @param {number} groupIndex Индекс группы
  */
-async function deleteGroup(userId, groupIndex) {
-  const connection = await pool.getConnection();
+async function deleteGroup (userId, groupIndex) {
+  const connection = await pool.getConnection()
 
   await connection.execute(
-    "DELETE FROM `contact_group` " +
-      "WHERE `contact_group`.`user_id` = ? AND `contact_group`.`idx` = ?",
-    [userId, groupIndex],
-  );
+    'DELETE FROM `contact_group` ' +
+      'WHERE `contact_group`.`user_id` = ? AND `contact_group`.`idx` = ?',
+    [userId, groupIndex]
+  )
 
   await connection.execute(
-    "UPDATE `contact_group` " +
-      "SET `contact_group`.`idx` = `contact_group`.`idx` - 1 " +
-      "WHERE `contact_group`.`user_id` = ? AND `contact_group`.`idx` > ?",
-    [userId, groupIndex],
-  );
+    'UPDATE `contact_group` ' +
+      'SET `contact_group`.`idx` = `contact_group`.`idx` - 1 ' +
+      'WHERE `contact_group`.`user_id` = ? AND `contact_group`.`idx` > ?',
+    [userId, groupIndex]
+  )
+}
+
+/**
+ * Редактировать никнейм контакта
+ *
+ * @param {number} contactId ID контакта
+ * @param {string} contactNickname Никнейм контакта
+ */
+async function modifyContactName (contactId, contactNickname) {
+  const connection = await pool.getConnection()
+
+  await connection.execute(
+    'UPDATE `contact` ' +
+      'SET `contact`.`nickname` = ? ' +
+      'WHERE `contact`.`id` = ?',
+    [contactNickname, contactId]
+  )
+}
+
+/**
+ * Удалить контакт
+ * @param {number} contactId ID контакта
+ */
+async function deleteContact (contactId) {
+  const connection = await pool.getConnection()
+
+  await connection.execute(
+    'DELETE FROM `contact` ' + 'WHERE `contact`.`id` = ?',
+    [contactId]
+  )
 }
 
 module.exports = {
@@ -286,4 +317,6 @@ module.exports = {
   searchUsers,
   modifyGroupName,
   deleteGroup,
-};
+  modifyContactName,
+  deleteContact
+}
